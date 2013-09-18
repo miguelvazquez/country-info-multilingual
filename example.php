0087d3786882f0563f627f44c34d991cef94521b
Peter Kahl <peter.kahl@colossalmind.com>
2013-09-19 06:50:31 +0800

    Added example of use.
    Added example of use. Added description using Chinese text.

---
+++ example.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ *
+ * Countries
+ * Example of use (PHP) 
+ * Copyright (c) 2013 Peter Kahl. All rights reserved.
+ * Use of this source code is governed by a GNU General Public License
+ * that can be found in the LICENSE file.
+ *
+ * https://github.com/peterkahl/countries
+ *
+ */
+
+
+
+define('DB_HOSTNAME', 'localhost');
+define('DB_DBNAME',   'someDBname');
+define('DB_USERNAME', 'someDBuser');
+define('DB_PASSWORD', 'someDBpwd');
+
+
+$mysqli = new mysqli(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DBNAME);
+
+if ($mysqli->connect_error) {
+	die('Connect Error ('.$mysqli->connect_errno.') '.$mysqli->connect_error);
+}
+
+$mysqli->set_charset("utf8");
+
+//----------------------------------------------------------------------
+
+$lang_code = 'zh_HK';
+
+$country_code = 'de';
+
+$sql = "SELECT * FROM `countries` WHERE `code`='".$country_code."' LIMIT 0,1";
+$result = $mysqli->query($sql);
+while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+	if ($lang_code == 'zh_HK') $lang_code = 'zh_TW';
+	$name = $row['country_'.$clang];
+}
+
+echo $name; // 德國
+
+//----------------------------------------------------------------------
+
+
